a44c9cb26ec59e834089a3541409fbf948ffb939
AndreiRadchenko <d4100kx@gmail.com>
2022-09-09 18:54:28 +0300

    start task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,15 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+function createGalleryMarkup(galleryItemsArray) {
+  return galleryItemsArray
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+              </a>`;
+    })
+    .join('');
+}
+
+galleryRef.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
