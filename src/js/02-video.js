604d6ca953d31b5a1580de326d20f62680e659ba
AndreiRadchenko <d4100kx@gmail.com>
2022-09-13 00:07:29 +0300

    done task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import localStorage from './storage';
 
+const PLAY_TIME_KEY = 'videoplayer-current-time';
+
+const playerRef = document.querySelector('#vimeo-player');
+
+const player = new Player(playerRef);
+
+function savePlayTime(data) {
+  localStorage.save(PLAY_TIME_KEY, Math.floor(data.seconds));
+}
+
+player.on('timeupdate', throttle(savePlayTime, 1000));
+
+let loadedTime = localStorage.load(PLAY_TIME_KEY);
+if (loadedTime) {
+  player.setCurrentTime(loadedTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import losalStorage from './storage';
 
+let num = 5;
+const bool = true;
+const str = 'my string';
+let arr = [1, 2, 3, 4, 5];
+const nullVar = null;
+let obj = {
+  num: 5,
+  bool: false,
+  str: 'my second string',
+  arr: ['a', 'b', 'c'],
+  nullVar: null,
+};
+
+losalStorage.save('object', obj);
+
+let jsonNum = JSON.stringify(num);
+console.log(typeof jsonNum);
+console.log(jsonNum);
+num = JSON.parse(jsonNum);
+console.log(typeof num);
+console.log(num);
+
+let jsonArr = JSON.stringify(arr);
+console.log(typeof jsonArr);
+console.log(jsonArr);
+arr = JSON.parse(jsonArr);
+console.log(typeof arr);
+console.log(arr);
+
+let jsonObj = JSON.stringify(obj);
+console.log(typeof jsonObj);
+console.log(jsonObj);
+obj = JSON.parse(jsonObj);
+console.log(typeof obj);
+console.log(obj);
+
+// console.log(JSON.stringify(bool));
+// console.log(JSON.stringify(str));
+// console.log(str);
